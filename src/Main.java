aa1cd00b32e70f75422a4713b6bee2a546f38db4
briz1 <my1queen8my1king@gmail.com>
2023-12-06 15:51:09 +0700

    Two exercises completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        int arcticBears;
+        arcticBears = 35200;
+        System.out.println("Значение переменной arcticBears с типом int равно " + arcticBears);
+        byte blackBook = 57;
+        System.out.println("Значение переменной blackBook с типом byte равно " + blackBook);
+        short cleanWindows = 30767;
+        System.out.println("Значение переменной cleanWindows с типом short равно " + cleanWindows);
+        long darkBricks = 30564000000L;
+        System.out.println("Значение переменной darkBricks с типом long равно " + darkBricks);
+        float energyInStock = 600500.4382f;
+        System.out.println("Значение переменной energyInStock с типом float равно " + energyInStock);
+        double freeMemory = 588402.9875731495;
+        System.out.println("Значение переменной freeMemory с типом double равно " + freeMemory);
+
+        System.out.println("Задача 2");
+        float a = 27.12f;
+        System.out.println("Значение переменной a с типом float равно " + a);
+        long b = 987678965549L;
+        System.out.println("Значение переменной b с типом long равно " + b);
+        double c = 2.786;
+        System.out.println("Значение переменной c с типом double равно " + c);
+        short d = 569;
+        System.out.println("Значение переменной d с типом short равно " + d);
+        int e = -159;
+        System.out.println("Значение переменной e с типом int равно " + e);
+        short f = 27897;
+        System.out.println("Значение переменной f с типом short равно " + f);
+        byte g = 67;
+        System.out.println("Значение переменной g с типом byte равно " + g);
+
+        System.out.println("Задача 3");
+
+
+
+
     }
 }
